e4cd1d0a99eb4d8230ca00cbb2ddab012156a939
Oliver343 <93383676+Oliver343@users.noreply.github.com>
2023-08-25 17:51:21 +0000

    added scale on cards

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Card (props) {
     return (
     <div className="card m-2 ">
-        <div className="card-header p-0"><img src={props.img}></img></div>
+        <img className="card-img-top" src={props.img}></img>
         <div className="card-body setSize text-centered"><h2>{props.title}</h2><p>{props.text}</p></div>
         <div className="card-footer d-flex justify-content-around"><button className="btn btn-primary">Find out more</button></div>
     </div>
